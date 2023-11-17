fa1c849c15454966d6106825d2885b667235818b
Ferry Febriansyah <perriebenhard@gmail.com>
2023-11-17 19:34:56 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          Ferry Febriansyah
+        </h2>
+        <p>Final Challenge Chapter 07</p>
       </header>
     </div>
   );
